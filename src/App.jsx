3ef1476af5b3df74e5de16e526ea16b930c2efe7
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-06-11 20:24:59 +0100

    Basic HTML markup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,43 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <div className="cardImg">
+          <img src="./src/images/illustration-hero.svg" alt="" className="image" />
+          <div className="cardBody">
+            <h1 className="cardHeader">
+              Order Summary
+            </h1>
+            <p className="description">
+            You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!
+            </p>
+            <div className="planPayment">
+              <img src="./src/images/icon-music.svg" alt="#" className="icon" />
+              <div className="planDet">
+                <p className="subHeader">
+                  Annual Plan
+                </p>
+                <p>
+                  $59.99/year
+                </p>
+                <a href="https://example.html">
+                  Change
+                </a>
+              </div>
+              <button className="proceed">
+                Proceed to Payment
+              </button>
+              <button className="cancel">
+                Cancel Order
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
